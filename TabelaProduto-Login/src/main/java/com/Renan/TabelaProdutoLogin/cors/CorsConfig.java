d67de37ddd9b6d8e199abfb2a612607c27159273
Renan Morais da Veiga <renanmoraisveiga@gmail.com>
2021-11-29 12:25:32 -0300

    .

--- TabelaProduto-Login/src/main/java/com/Renan/TabelaProdutoLogin/cors/CorsConfig.java
+++ TabelaProduto-Login/src/main/java/com/Renan/TabelaProdutoLogin/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.Renan.TabelaProdutoLogin;
+package com.Renan.TabelaProdutoLogin.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
